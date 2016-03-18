b822cffed171c37eab01ec0f31e6a43380b00460
terigen <teruun@163.com>
2016-03-18 13:05:41 +0800

    13-1

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -7,7 +7,15 @@ Template.postItem.helpers({
     a.href = this.url;
     return a.hostname;
   },
+
   // commentsCount: function() {
   //   return Comments.find({postId: this._id}).count();
   // }
+});
+
+Template.postItem.events({
+  'click .upvote': function(e) {
+    e.preventDefault();
+    Meteor.call('upvote', this._id);
+  }
 });
--- server/fixtures.js
+++ server/fixtures.js
@@ -18,7 +18,9 @@ if (Posts.find().count() === 0) {
     author: sacha.profile.name,
     url: 'http://sachagreif.com/introducing-telescope/',
     submitted: new Date(now - 7 * 3600 * 1000),
-    commentsCount: 2
+    commentsCount: 2,
+    upvoters: [],
+    votes: 0
   });
 
   Comments.insert({
@@ -36,13 +38,16 @@ if (Posts.find().count() === 0) {
     submitted: new Date(now - 3 * 3600 * 1000),
     body: 'You sure can Tom!'
   });
+
   Posts.insert({
     title: 'Meteor',
     userId: tom._id,
     author: tom.profile.name,
     url: 'http://meteor.com',
     submitted: new Date(now - 10 * 3600 * 1000),
-    commentsCount: 0
+    commentsCount: 0,
+    upvoters: [],
+    votes: 0
   });
 
   Posts.insert({
@@ -51,16 +56,9 @@ if (Posts.find().count() === 0) {
     author: tom.profile.name,
     url: 'http://themeteorbook.com',
     submitted: new Date(now - 12 * 3600 * 1000),
-    commentsCount: 0
-  });
-
-  Posts.insert({
-    title: 'The Meteor Book',
-    userId: tom._id,
-    author: tom.profile.name,
-    url: 'http://themeteorbook.com',
-    submitted: new Date(now - 12 * 3600 * 1000),
-    commentsCount: 0
+    commentsCount: 0,
+    upvoters: [],
+    votes: 0
   });
 
   for (var i = 0; i < 10; i++) {
@@ -69,8 +67,10 @@ if (Posts.find().count() === 0) {
       author: sacha.profile.name,
       userId: sacha._id,
       url: 'http://google.com/?q=test-' + i,
-      submitted: new Date(now - i * 3600 * 1000),
-      commentsCount: 0
+      submitted: new Date(now - i * 3600 * 1000 + 1),
+      commentsCount: 0,
+      upvoters: [],
+      votes: 0
     });
   }
 }
